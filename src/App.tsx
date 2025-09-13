7b8e8744e2c509c344851b2cdf34bd353f14b793
aymen-Dahmoun <am.dahmoun@ensta.edu.dz>
2025-09-13 03:04:46 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='flex justify-center items-center w-screen h-screen m-0 p-0 bg-amber-300' >
+      <span className='text-amber-900 text-8xl' >hello</span>
+    </div>
+  )
+}
+
+export default App
