2e48a0f7167224de027b7d9033a161676cd0a6e2
aymen-Dahmoun <am.dahmoun@ensta.edu.dz>
2025-09-19 07:47:32 +0200

    removed tsx extenssions

--- src/components/TechIUse.tsx
+++ src/components/TechIUse.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState, type JSX } from "react";
-import TechCard from "../components/TechCard.tsx";
+import TechCard from "../components/TechCard";
 import {
   SiReact,
   SiRedux,
