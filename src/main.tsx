94b684dd537a5372af84020528b96a8458b6826f
DAHMOUN Mouaine Aymen <am.dahmoun@ensta.edu.dz>
2026-06-10 20:31:52 +0100

    feat: added vercel analystics

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import { HelmetProvider } from 'react-helmet-async'
 import './index.css'
 import './i18n'
 import App from './App'
+import { Analytics } from "@vercel/analytics/next"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HelmetProvider>
       <App />
+      <Analytics />
     </HelmetProvider>
   </StrictMode>,
 )
